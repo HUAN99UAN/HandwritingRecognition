a4abe8dd5c7130ef59d58e1efff2fa408ca781be
Laura Baakman <laurabaakman@gmail.com>
2016-05-12 13:32:58 +0200

    Code to create the path to the image file.

--- cropper/main.py
+++ cropper/main.py
@@ -1,6 +1,9 @@
 import argparse
+import os.path
 
 import wordsFile
+
+_image_file_extension = 'jpg'
 
 
 def parse_command_line_arguments():
@@ -10,6 +13,15 @@ def parse_command_line_arguments():
     parser.add_argument("imageDirectory", help="the path to the directory with images")
     args = parser.parse_args()
     return args.wordsFile, args.imageDirectory
+
+
+def build_file_name(file_name, extension):
+    return '.'.join([file_name, extension])
+
+
+def build_file_path(path, file_name, extension=''):
+    return os.path.join(path, build_file_name(file_name, extension))
+
 
 if __name__ == "__main__":
     (words_file, image_directory) = parse_command_line_arguments()
