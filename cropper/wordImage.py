06b63abd45450c1f7117d2c78d18222b9d504daf
Laura Baakman <laurabaakman@gmail.com>
2016-05-13 17:23:00 +0200

    Word images are extracted.

--- cropper/wordImage.py
+++ cropper/wordImage.py
@@ -5,12 +5,16 @@ class WordImage:
     def __init__(self, number, line_image, tree):
         self._line_image = line_image
         self._tree = tree
-        self.image = self._extract_word_image()
         self._number = number
+        self._text = self._tree.get_text()
+        self.image = self._extract_word_image()
         self._characters = self._build_character_dict()
 
     def _extract_word_image(self):
-        word_image = None
+        # TODO nasty, fix this!
+        page_image_copy = self._line_image._page_image.image.copy()
+        bounding_box = self._tree.get_bounding_box()
+        word_image = page_image_copy.crop(bounding_box)
         return word_image
 
     def _build_character_dict(selfself):
@@ -21,7 +25,8 @@ class WordImage:
     def __str__(self):
         return ", ".join([
             "{WordImage",
-            "number: " + self._number,
-            "number of characters: " + str(len(self._words)),
+            "number: " + str(self._number),
+            "text: " + self._text,
+            "number of characters: " + str(len(self._characters)),
             "}"
         ])
