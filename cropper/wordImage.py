296506df4f89fadc28329c9d0bdab6780c51c103
Laura Baakman <laurabaakman@gmail.com>
2016-05-13 16:54:24 +0200

    Set up of word_image class.

---
+++ cropper/wordImage.py
@@ -0,0 +1,33 @@
+import annotationTree
+
+class WordImage:
+
+    def __init__(self, number, line_image, tree):
+        self._line_image = line_image
+        self._tree = tree
+        self.image = self._extract_word_image()
+        self._number = number
+        self._characters = self._build_character_dict()
+
+
+    def __getattr__(self, item):
+        if item == '_image':
+            raise AttributeError()
+        return getattr(self.image, item)
+
+    def _extract_word_image(self):
+        word_image = None
+        return word_image
+
+    def _build_character_dict(selfself):
+        characters = dict()
+        print("_build_character_dict")
+        return characters
+
+    def __str__(self):
+        return ", ".join([
+            "{WordImage",
+            "number: " + self._number,
+            "number of characters: " + str(len(self._words)),
+            "}"
+        ])
