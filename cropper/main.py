7087a31b44d558f42b9b4c3fa57aaae3776dae5a
Laura Baakman <laurabaakman@gmail.com>
2016-05-13 19:14:44 +0200

    We can know handle wildcards, as long as we don't use pycharm to call the script.

--- cropper/main.py
+++ cropper/main.py
@@ -10,10 +10,13 @@ _image_file_extension = 'jpg'
 def parse_command_line_arguments():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("wordsFile", help="the path to the words file")
-    parser.add_argument("imageDirectory", help="the path to the directory with images")
+    parser.add_argument("imageDirectory", type=str,
+                        help="the path to the directory with images")
+    parser.add_argument("wordsFiles", nargs='+', type=str,
+                        help="the words files, should be at least one file. Each words file should be associated with "
+                             "an image in the imageDirectory.")
     args = parser.parse_args()
-    return args.wordsFile, args.imageDirectory
+    return args.wordsFiles, args.imageDirectory
 
 
 def build_file_name(file_name, extension):
@@ -25,12 +28,12 @@ def build_file_path(path, file_name, extension=''):
 
 
 if __name__ == "__main__":
-    (words_file, image_directory) = parse_command_line_arguments()
-    words_file = annotationTree.AnnotationTree(file_path=words_file)
-    words_file.get_image_file_name()
-    image_file_path = build_file_path(
-        path=image_directory,
-        file_name=words_file.get_image_file_name(),
-        extension=_image_file_extension
-    )
-    page_image = pageImage.PageImage(image_file_path, words_file)
+    (words_files, image_directory) = parse_command_line_arguments()
+    for words_file in words_files:
+        tree = annotationTree.AnnotationTree(file_path=words_file)
+        image_file_path = build_file_path(
+            path=image_directory,
+            file_name=tree.get_image_file_name(),
+            extension=_image_file_extension
+        )
+        page_image = pageImage.PageImage(image_file_path, tree)
