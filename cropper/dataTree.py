cc84a792157dd589aa10d53efb5918f551db60cc
Laura Baakman <laurabaakman@gmail.com>
2016-05-15 15:19:27 +0200

    Documentation.

--- cropper/dataTree.py
+++ cropper/dataTree.py
@@ -1,19 +1,27 @@
 class Node:
     """
     Node of a tree.
-
-    Arguments:
-        *description* description of the node
-        *parent* parent of the node
-        *children* dictionary with the description of the children as key, and the children as values.
     """
-    def __init__(self, **kwargs):
+    def __init__(self, kwargs):
+        """
+        Constructor of the Node class
+        :param kwargs: with the following keys:
+            *description* description of the node
+            *parent* parent of the node
+            *children* dictionary with the description of the children as key, and the children as values.
+        """
         super(Node, self).__init__()
         self._description = kwargs.get('description')
         self.parent = kwargs.get('parent')
         self.children = kwargs.get('children')
 
     def get_root(self):
+        """
+        Get the root of the tree this node is part of.
+
+        :rtype: dataTree.Node
+        :return: the root node of the tree.
+        """
         return self.parent.get_root()
 
     def _repr_description(self):
@@ -38,12 +46,14 @@ class Node:
 class Root(Node):
     """
     Root of a tree.
-
-    Arguments
-        *description* description of the node
-        *children* dictionary with the description of the children as key, and the children as values.
     """
     def __init__(self, **kwargs):
+        """
+        Constructor of the Root class
+        :param kwargs: with the following keys:
+            *description* description of the node
+            *children* dictionary with the description of the children as key, and the children as values.
+        """
         kwargs['parent'] = None
         super(Root, self).__init__(**kwargs)
 
@@ -63,12 +73,14 @@ class Root(Node):
 class Leaf(Node):
     """
     Node of a tree.
-
-    Arguments:
-        *description* description of the node
-        *parent* parent of the node
     """
     def __init__(self, **kwargs):
+        """
+        Constructor of the Root class
+        :param kwargs: with the following keys:
+            *description* description of the node
+            *parent* parent of the node
+        """
         kwargs['children'] = None
         super(Leaf, self).__init__(**kwargs)
 
