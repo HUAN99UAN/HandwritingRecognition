35d8daf03c4a23d8d894937f6db7b3ffa98706e2
Laura Baakman <laurabaakman@gmail.com>
2016-05-16 13:28:54 +0200

    Move the image opener from the dead pageImage class to the dataset class.

--- cropper/dataset.py
+++ cropper/dataset.py
@@ -1,9 +1,10 @@
 from builtins import staticmethod
+import os.path
+
+from PIL import Image
 
 import annotationTree
 import inputElements
-import pageImage
-import os.path
 
 
 class DataSet:
@@ -70,7 +71,7 @@ class DataSetBuilder:
             tree = annotationTree.AnnotationTree(file_path=words_file)
             image_file_name = tree.get_image_file_name()
             image_file_path = self._build_image_file_path(image_file_name,)
-            image = pageImage.ImageOpener(image_file_path).open()
+            image = ImageOpener(image_file_path).open()
             page_image = inputElements.PageImage(
                 description=image_file_name,
                 image=image,
@@ -78,3 +79,23 @@ class DataSetBuilder:
             )
             data_set.add(page_image, image_file_name)
         return data_set
+
+class ImageOpener:
+
+    def __init__(self, image_file_path):
+        self.image_file_path = image_file_path
+
+    def _file_can_be_opened(self):
+        return \
+            os.path.exists(self.image_file_path) and \
+            os.path.isfile(self.image_file_path)
+
+    def open(self):
+        image = None
+        if self._file_can_be_opened():
+            try:
+                image = Image.open(self.image_file_path)
+            except IOError:
+                import sys
+                sys.stderr.write("Could not read the file {}".format(self.image_file_path))
+        return image
--- cropper/pageImage.py
+++ cropper/pageImage.py
@@ -57,22 +57,3 @@ class PageImage(tree.Root):
             "}"])
 
 
-class ImageOpener:
-
-    def __init__(self, image_file_path):
-        self.image_file_path = image_file_path
-
-    def _file_can_be_opened(self):
-        return \
-            os.path.exists(self.image_file_path) and \
-            os.path.isfile(self.image_file_path)
-
-    def open(self):
-        image = None
-        if self._file_can_be_opened():
-            try:
-                image = Image.open(self.image_file_path)
-            except IOError:
-                import sys
-                sys.stderr.write("Could not read the file {}".format(self.image_file_path))
-        return image
