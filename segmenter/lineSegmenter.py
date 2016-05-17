52075da22da8e6ddd9806f46e503308bceecf8e5
Laura Baakman <laurabaakman@gmail.com>
2016-05-17 17:06:07 +0200

    Set up of the line segmenter class.

---
+++ segmenter/lineSegmenter.py
@@ -0,0 +1,10 @@
+
+class lineSegmenter:
+    """
+    Class that segments lines based on the algorithm described in Tripathy, Nilamadhaba, and Umapada Pal. "Handwriting
+    segmentation of unconstrained Oriya text." Sadhana 31.6 (2006): 755-769.
+    """
+
+    def __init__(self, image):
+        self._image = image
+
