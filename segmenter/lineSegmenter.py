449b9622d4d7cae61cfb781f1eb5f2ed5ce9b2f7
Laura Baakman <laurabaakman@gmail.com>
2016-05-17 18:01:56 +0200

    Introduced a stroke class that returns a list of stroke widths.

--- segmenter/lineSegmenter.py
+++ segmenter/lineSegmenter.py
@@ -1,3 +1,5 @@
+import math
+
 
 class LineSegmenter:
     """
@@ -7,4 +9,37 @@ class LineSegmenter:
 
     def __init__(self, image):
         self._image = image
+        self._strokes = self._compute_strokes()
 
+    def _compute_strokes(self):
+        strokes = list()
+        stroke_width = Stroke.compute_width()
+        # image_width = self._image.size[1]
+        image_width = 10
+        for stroke_width in Stroke.stroke_widths(stroke_width, image_width):
+            print(stroke_width)
+        return strokes
+
+    def _number_of_strokes(self, stroke_width):
+        image_width = self._image.size[1]
+        return math.ceil(image_width / stroke_width)
+
+
+class Stroke:
+    """
+    Representation of the strokes in the image used for the line segmentation.
+    """
+
+    def __init__(self, width):
+        self._width = width
+
+    @staticmethod
+    def stroke_widths(stroke_width, image_width):
+        number_of_normal_strokes = math.floor(image_width / stroke_width)
+        odd_stroke_size = image_width % stroke_width
+        odd_stroke = [odd_stroke_size] if odd_stroke_size is not 0 else list()
+        return ([stroke_width] * number_of_normal_strokes) + odd_stroke
+
+    @staticmethod
+    def compute_width():
+        return 3
