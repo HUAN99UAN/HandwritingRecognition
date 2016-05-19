21547c8ecb62c439224a746db208eff358769ac8
Laura Baakman <laurabaakman@gmail.com>
2016-05-19 14:43:30 +0200

    Method to compute the mode of a iterable or mapping.

--- segmenter/utils.py
+++ segmenter/utils.py
@@ -1,6 +1,14 @@
 from collections import namedtuple
-from typing import TypeVar
-
-IntFloat = TypeVar('IntFloat', int, float)
 
 Point = namedtuple('Point', ['x', 'y'])
+
+
+def mode(lst):
+    """
+    Returns the most frequent value of the list.
+    :param lst: the iterable or mapping from which the most frequent value is to be computed.
+    :return: the moest frequent value.
+    """
+    from collections import Counter
+    counter = Counter(lst)
+    return counter.most_common(1)[0]
