598abc6b2417ee1c7fa548e97a5c41fa2b862288
Angelo404 <e.karountzos@hotmail.com>
2016-05-23 21:13:32 +0200

    crossing feature

--- noiseRemover/characterFeatureExtraction.py
+++ noiseRemover/characterFeatureExtraction.py
@@ -10,8 +10,8 @@ class characterFeatureExtraction:
 	def __init__(self):
 		self._th_fltr = thresholdFilter()
 
-	def load_char(self):
-		return cv2.imread("alpha1.png",0)
+	def load_char(self, name):
+		return cv2.imread(name,0)
 
 	def clear_char(self, img):
 		return self._th_fltr.global_threshold(img, 150, 1)
@@ -19,10 +19,15 @@ class characterFeatureExtraction:
 	def split_regions(self, img, R=3):
 		return np.split(img, R, axis=1)
 
-	def horizontal_celled_prj_feature(self, regions, rows):
-		 
-		vector = np.zeros(rows*len(regions))
+	def horizontal_celled_prj_feature(self, regions):
+
+		total_rows = 0
 		counter = 0
+
+		for r in regions:
+			total_rows = total_rows + len(r)
+		 
+		vector = np.zeros(total_rows)
 
 		for r in regions:
 			for row in r:
@@ -30,9 +35,22 @@ class characterFeatureExtraction:
 					if c == 1:
 						vector[counter] = 1
 						break
-				counter = counter +1
+				counter = counter + 1
 
-		return vector.reshape(3,51)
+		return vector.reshape(len(regions),total_rows/len(regions))
+
+	def crossing_feature(self, img):
+		size = img.shape
+
+		vector = np.zeros(size[0])
+		print vector
+		for r in range(len(img)):
+			for c in xrange(1, len(img[r]), 1):
+				if img[r][c-1] != img[r][c]:
+					vector[r] = vector[r]+1
+
+		return vector
+
 
 	def invert_image(self, img):
 
@@ -48,24 +66,21 @@ class characterFeatureExtraction:
 if __name__ == '__main__':
 	c = characterFeatureExtraction()
 
-	tmp = c.clear_char(c.load_char())
-
-	z = np.ones((51,1))
-
-	for i in xrange(6):
-		tmp = np.append(tmp, z, axis=1)
+	tmp = c.clear_char(c.load_char("alpha1.png"))
 
 	tmp = c.invert_image(tmp)
 
 	regions = c.split_regions(tmp)
 
-	print c.horizontal_celled_prj_feature(regions, tmp.shape[1])
+	#print c.horizontal_celled_prj_feature(regions)
+
+	print c.crossing_feature(tmp)
 
 	# x = cluster.vq.kmeans(cluster.vq.whiten(smth), 5)
 
 	# print x
 
-	# plt.imshow(tmp, 'gray')
-	# plt.show()
+	plt.imshow(tmp, 'gray')
+	plt.show()
 
 
