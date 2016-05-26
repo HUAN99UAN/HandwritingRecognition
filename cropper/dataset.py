b2d21d6cea6bd81969c3c99148639a3393549cd5
Laura Baakman <laurabaakman@gmail.com>
2016-05-26 17:15:12 +0200

    Improved the file opener in dataset.

--- cropper/dataset.py
+++ cropper/dataset.py
@@ -1,5 +1,6 @@
 from builtins import staticmethod
 import os.path
+import sys
 
 from PIL import Image
 
@@ -70,6 +71,7 @@ class DataSetBuilder:
         for words_file in self._words_files:
             tree = annotationTree.AnnotationTree(file_path=words_file)
             image_file_name = tree.image_file_name
+            print(image_file_name)
             image_file_path = self._build_image_file_path(image_file_name)
             image = ImageOpener(image_file_path).open()
             page_image = inputElements.PageImage(
@@ -81,22 +83,34 @@ class DataSetBuilder:
         return data_set
 
 
+class NonExistentFileError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
 class ImageOpener:
 
     def __init__(self, image_file_path):
         self.image_file_path = image_file_path
 
-    def _file_can_be_opened(self):
-        return \
-            os.path.exists(self.image_file_path) and \
-            os.path.isfile(self.image_file_path)
+    def verify_image_file(self):
+        if not (os.path.exists(self.image_file_path) and os.path.isfile(self.image_file_path)):
+            raise NonExistentFileError(
+                "The file {} cannot be found.".format(
+                    self.image_file_path)
+            )
 
     def open(self):
-        image = None
-        if self._file_can_be_opened():
-            try:
-                image = Image.open(self.image_file_path)
-            except IOError:
-                import sys
-                sys.stderr.write("Could not read the file {}".format(self.image_file_path))
+        try:
+            self.verify_image_file()
+            image = Image.open(self.image_file_path)
+        except IOError:
+            sys.stderr.write("Could not read the file {}".format(self.image_file_path))
+            sys.exit(-1)
+        except NonExistentFileError as error:
+            sys.stderr.write(str(error))
+            sys.exit(-1)
         return image
