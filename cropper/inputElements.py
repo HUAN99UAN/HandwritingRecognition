700cd5ca3623683307c7558071f245f1ea3b85c6
Laura Baakman <laurabaakman@gmail.com>
2016-05-26 12:07:30 +0200

    Renamed variable.

--- cropper/inputElements.py
+++ cropper/inputElements.py
@@ -60,9 +60,9 @@ class PageElementImage:
     def _extract_sub_image(self):
         # The PIL documentation is vague about whether or not cropped images are cropped copies of the original
         # image, or new images. Just to be safe they are copied.
-        source_image_copy = self.root.image
+        source_image = self.root.image
         bounding_box = self._tree.get_bounding_box()
-        return source_image_copy.crop(bounding_box)
+        return source_image.crop(bounding_box)
 
     def lines(self):
         return list()
