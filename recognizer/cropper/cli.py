6b965166ec72dee5225f1135d820c8f386b75199
Laura Baakman <laurabaakman@gmail.com>
2016-05-26 20:43:29 +0200

    Moved the actions to a shared directory.

--- recognizer/cropper/cli.py
+++ recognizer/cropper/cli.py
@@ -1,6 +1,4 @@
 import argparse
-from glob import glob
-import os
 import sys
 
 # Fix path issues
@@ -10,40 +8,10 @@ sys.path.append(root)
 
 from PIL import Image
 
+from utils.actions import VerifyOutputExtensionAction, ExpandWordFilesPathsAction
 import dataset
 
 default_output_extension = "jpg"
-
-
-class VerifyOutputExtensionAction(argparse.Action):
-    @staticmethod
-    def _is_valid_output_extension(extension):
-        return extension.upper() in Image.SAVE.keys()
-
-    def __call__(self, parser, namespace, values, option_string=None):
-        if not VerifyOutputExtensionAction._is_valid_output_extension(values):
-            raise ValueError("The extension {} is  not supported.".format(values))
-        setattr(namespace, self.dest, values)
-
-
-class ExpandWordFilesPathsAction(argparse.Action):
-    @staticmethod
-    def _expand_wild_card(values):
-        paths = list()
-        if isinstance(values, list):
-            [paths.extend(glob(value)) for value in values]
-        else:
-            paths = list(glob(values))
-        return paths
-
-    @staticmethod
-    def _to_absolute_path(paths):
-        return [os.path.abspath(path) for path in paths]
-
-    def __call__(self, parser, namespace, values, option_string=None):
-        values = ExpandWordFilesPathsAction._expand_wild_card(values)
-        values = ExpandWordFilesPathsAction._to_absolute_path(values)
-        setattr(namespace, self.dest, values)
 
 
 def parse_command_line_arguments():
