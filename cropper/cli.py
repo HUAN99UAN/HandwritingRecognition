62dda187bbd09267dd25f3b3c3545678c8afcf4b
Laura Baakman <laurabaakman@gmail.com>
2016-05-26 10:20:04 +0200

    expand the wildcard

--- cropper/cli.py
+++ cropper/cli.py
@@ -1,4 +1,6 @@
 import argparse
+from glob import glob
+
 from PIL import Image
 
 import dataset
@@ -9,12 +11,25 @@ default_output_extension = "jpg"
 class VerifyOutputExtensionAction(argparse.Action):
     @staticmethod
     def _is_valid_output_extension(extension):
-        Image.init()
         return extension.upper() in Image.SAVE.keys()
 
     def __call__(self, parser, namespace, values, option_string=None):
         if not VerifyOutputExtensionAction._is_valid_output_extension(values):
             raise ValueError("The extension {} is  not supported.".format(values))
+        setattr(namespace, self.dest, values)
+
+
+class ExpandPathsAction(argparse.Action):
+    @staticmethod
+    def _expand_wild_card(values):
+        if isinstance(values, list):
+            values = [glob(value) for value in values]
+        else:
+            values = glob(values)
+        return values
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        values = self._expand_wild_card(values)
         setattr(namespace, self.dest, values)
 
 
@@ -26,7 +41,7 @@ def parse_command_line_arguments():
     parser.add_argument('outputDirectory', type=str,
                         help='The path to the directory where you want to store the folders that represent pages. If '
                              'the folder does not exist it is created for you.')
-    parser.add_argument('wordsFiles', nargs='+', type=str,
+    parser.add_argument('wordsFiles', nargs='+', type=str, action=ExpandPathsAction,
                         help='The words files, should be at least one file. Each words file should be associated with '
                              'an image in the imageDirectory.')
     parser.add_argument('--outputExtension', type=str, default=default_output_extension,
@@ -36,10 +51,13 @@ def parse_command_line_arguments():
     return parser.parse_args()
 
 if __name__ == "__main__":
+    Image.init()
     cli_arguments = parse_command_line_arguments()
-    data_set = dataset.DataSet.from_files(
-        cli_arguments.wordsFiles,
-        cli_arguments.imageDirectory)
-    data_set.to_cropped_images_hierarchy(
-        directory=cli_arguments.outputDirectory,
-        extension=cli_arguments.outputExtension)
+
+    print(cli_arguments)
+    # data_set = dataset.DataSet.from_files(
+    #     cli_arguments.wordsFiles,
+    #     cli_arguments.imageDirectory)
+    # data_set.to_cropped_images_hierarchy(
+    #     directory=cli_arguments.outputDirectory,
+    #     extension=cli_arguments.outputExtension)
