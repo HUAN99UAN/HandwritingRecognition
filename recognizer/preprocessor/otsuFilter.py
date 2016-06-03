8c24f4795a8d38ca87ca85dd44eb75cee72d16e9
Laura Baakman <laurabaakman@gmail.com>
2016-06-03 18:10:50 +0200

    threshold expects a unit8 gray scale image.

--- recognizer/preprocessor/otsuFilter.py
+++ recognizer/preprocessor/otsuFilter.py
@@ -1,14 +1,13 @@
-
-
 import cv2
-
+import numpy as np
 
 class otsuFilter:
 	def __init__(self):
 		pass
 
 	def otsu_threshold(self, img, threshold = 0):
-		_, img_after_otsu = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+		img_grey = cv2.cvtColor(img.astype(np.uint8), cv2.COLOR_BGR2GRAY)
+		_, img_after_otsu = cv2.threshold(img_grey,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 		return img_after_otsu
 
 if __name__ == '__main__':
