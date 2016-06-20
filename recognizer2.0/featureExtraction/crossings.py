c08148d9922b3d84610d37a373f5280e7d3ae6b6
Laura Baakman <laurabaakman@gmail.com>
2016-06-20 17:38:40 +0200

    Completed the feature extractor that uses crossings.

--- recognizer2.0/featureExtraction/crossings.py
+++ recognizer2.0/featureExtraction/crossings.py
@@ -1,5 +1,7 @@
 import featureExtraction.interface as interface
-from utils.things import Size
+from preprocessing.colorspaces import ToBinary
+
+import numpy as np
 
 
 class Crossings(interface.AbstractFeatureExtractor):
@@ -9,17 +11,42 @@ class Crossings(interface.AbstractFeatureExtractor):
     character recognition."
     """
 
-    _default_number_of_features = 128
+    _default_number_of_vertical_features = 64
+    _default_number_of_horizontal_features = 64
 
-    def __init__(self, number_of_features=_default_number_of_features):
+    def __init__(self,
+                 number_of_vertical_features=_default_number_of_vertical_features,
+                 number_of_horizontal_features=_default_number_of_horizontal_features):
         super(Crossings, self).__init__()
-        self._number_of_features = number_of_features
+        self._number_of_vertical_features = number_of_vertical_features
+        self._number_of_horizontal_features = number_of_horizontal_features
+
+    @property
+    def number_of_features(self):
+        return self._number_of_horizontal_features + self._number_of_vertical_features
 
     def extract(self, image):
-        resized_image = image.resize(width=self._number_of_features)
-        # Resize image to self.extraction_size
-        # Invert image
-        # Comput crossing feature
+        feature_vector_horizontal = self._count_crossings(
+            image, self._number_of_horizontal_features, axis=1
+        )
+        feature_vector_vertical= self._count_crossings(
+            image, self._number_of_horizontal_features, axis=0
+        )
+        return np.asarray(np.hstack((feature_vector_horizontal, feature_vector_vertical)), dtype=np.float)
+
+    @classmethod
+    def _scale_image(cls, image, new_height):
+        scaled_image = image.resize(height=new_height)
+        if not scaled_image.color_mode.is_binary:
+            scaled_image = ToBinary().apply(scaled_image)
+        return scaled_image
+
+    @classmethod
+    def _count_crossings(self, image, number_of_features, axis):
+        scaled_image = self._scale_image(image, number_of_features)
+        differences = np.diff(scaled_image, axis=axis)
+        differences[differences != 0] = 1
+        return np.sum(differences, axis=axis)
 
     def __repr__(self):
         return "%s(%r)" % (self.__class__, self.__dict__)
