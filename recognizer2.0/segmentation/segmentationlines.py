1a80a47e3ffa63b4842218a0601479cc4dafc788
Laura Baakman <laurabaakman@gmail.com>
2016-06-21 18:01:05 +0200

    Wrapper for segmentation lines.

---
+++ recognizer2.0/segmentation/segmentationlines.py
@@ -0,0 +1,30 @@
+from utils.shapes import VerticalLine
+
+import numpy as np
+
+
+class SegmentationLines(object):
+    """Object to collect segmentation lines, basically a wrapper around a list."""
+
+    def __init__(self, lines):
+        super(SegmentationLines, self).__init__()
+        self._lines= lines
+
+    def lines(self):
+        for line in self._lines:
+            yield line
+
+    def paint_on(self, image, color=(0, 0, 0), width=10):
+        for line in self._lines:
+            image = line.paint_on(image, color=color, width=width)
+        return image
+
+    @staticmethod
+    def from_x_coordinates(x_coordinates, image_height):
+        lines = list()
+        for x in np.nditer(x_coordinates):
+            lines.append(VerticalLine(x=x, y1=0, y2=image_height))
+        return SegmentationLines(lines)
+
+    def __repr__(self):
+        return "%s(%r)" % (self.__class__, self.__dict__)
