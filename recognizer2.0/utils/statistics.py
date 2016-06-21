006ef2322d96ab7252435742b71f9e6d99a2b1d4
Laura Baakman <laurabaakman@gmail.com>
2016-06-21 14:55:29 +0200

    Added a mode function.

---
+++ recognizer2.0/utils/statistics.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+
+def mode(values):
+    most_frequent_values = Counter(values)
+    most_frequent_value, _ = most_frequent_values.most_common(1)[0]
+    return most_frequent_value
