2002ddc0971bdc62884ba74b8915453dbe81b61f
Laura Baakman <laurabaakman@gmail.com>
2016-06-23 12:21:59 +0200

    Completed the hole filter.

--- recognizer2.0/segmentation/binaryoversegmentation/segmentationlinesfilters.py
+++ recognizer2.0/segmentation/binaryoversegmentation/segmentationlinesfilters.py
@@ -19,6 +19,7 @@ class _AbstractSegmentationLineFilter(object):
     def __repr__(self):
         return "%s(%r)" % (self.__class__, self.__dict__)
 
+
 class HoleFilter(object):
     """Return false for the segmentation lines that lie in a hole."""
 
@@ -35,13 +36,26 @@ class HoleFilter(object):
     def _compute_contour_image(self):
         image = Invert().apply(copy.deepcopy(self.image))
         contours, hierarchy = cv2.findContours(image=image, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
-        contour_image = Image(np.ones(self.image.shape), color_mode=ColorMode.binary)
-        cv2.drawContours(contour_image, contours=contours, contourIdx=6, color=(0, 0, 0), thickness=-1)
-        contour_image.show(wait_key=0)
+        inner_contours = self._find_inner_contours(hierarchy)
+        contour_image = self._add_contours_to_image(contours, inner_contours)
+        return contour_image
+
+    def _find_inner_contours(self, hierarchy):
+        inner_contours = list()
+        for contour, idx in zip(hierarchy[0], range(len(hierarchy[0]))):
+            if contour[3] != -1:
+                inner_contours.append(idx)
+        return inner_contours
+
+    def _add_contours_to_image(self, contours, contour_idx, contour_image=None):
+        if not contour_image:
+            contour_image = Image(np.ones(self.image.shape), color_mode=ColorMode.binary)
+        for idx in contour_idx:
+            cv2.drawContours(contour_image, contours=contours, contourIdx=idx, color=(0, 0, 0), thickness=-1)
         return contour_image
 
     def keep(self, segmentation_line):
-        raise NotImplementedError()
+        return not self._hole_bits[segmentation_line.x]
 
     def __repr__(self):
         return "%s(%r)" % (self.__class__, self.__dict__)
