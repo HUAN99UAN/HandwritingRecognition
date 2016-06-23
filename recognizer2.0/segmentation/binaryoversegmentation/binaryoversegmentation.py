6eab8c733c9ed42f09ce3b3a49fa1f1862d6a232
Laura Baakman <laurabaakman@gmail.com>
2016-06-23 11:30:43 +0200

    Small changes.

--- recognizer2.0/segmentation/binaryoversegmentation/binaryoversegmentation.py
+++ recognizer2.0/segmentation/binaryoversegmentation/binaryoversegmentation.py
@@ -3,8 +3,10 @@ import segmentation.binaryoversegmentation.strokewidth as strokewidth
 import segmentation.interface
 from postprocessing.lexicon import Lexicon
 from segmentation.binaryoversegmentation.suspiciousregions import SuspiciousRegionsComputer
-from segmentationlines import SegmentationLines
+import segmentation.binaryoversegmentation.segmentatoinlinesfilters as filters
 from utils.image import Image
+from utils.shapes import Rectangle
+from utils.things import Point
 
 red = (0, 0, 255)
 blue = (255, 0, 0)
@@ -34,16 +36,30 @@ class BinaryOverSegmentation(segmentation.interface.AbstractSegmenter):
 
         self._log = dict()
 
+    def extract_body_bounding_box(self, image):
+        return Rectangle(
+            top_left=Point(x=0, y=self._high_base_line.y),
+            bottom_right=Point(x=image.width, y=self._low_base_line.y)
+        )
+
     def segment(self, image):
         self._low_base_line, self._high_base_line = self._base_line_estimator.estimate(image)
         self._stroke_width = self._stroke_width_estimator.estimate(image)
-        suspicious_regions = SuspiciousRegionsComputer(threshold=self._stroke_width * 2).compute(image)
+
+        body_region = image.sub_image(bounding_box=self.extract_body_bounding_box(image))
+
+        suspicious_regions = SuspiciousRegionsComputer(threshold=self._stroke_width * 2).compute(body_region)
         self._log['suspicious_regions'] = suspicious_regions
 
         segmentation_lines = suspicious_regions.to_segmentation_lines(stroke_width=self._stroke_width)
         self._log['initial_segmentation_lines'] = segmentation_lines
 
-        
+        self._filter_segmentation_lines(image=image, segmentation_lines=segmentation_lines)
+
+    def _filter_segmentation_lines(self, image, segmentation_lines):
+        hole_filter = filters.HoleFilter(image=image)
+        for line in segmentation_lines.lines:
+            hole_filter.keep(line)
 
     def __repr__(self):
         return "%s(%r)" % (self.__class__, self.__dict__)
@@ -52,7 +68,7 @@ class BinaryOverSegmentation(segmentation.interface.AbstractSegmenter):
 if __name__ == '__main__':
     from preprocessing.colorspaces import ToBinary
 
-    image_file = '/Users/laura/Repositories/HandwritingRecognition/data/testdata/word.png'
+    image_file = '/Users/laura/Repositories/HandwritingRecognition/data/testdata/word_2.png'
     image = Image.from_file(image_file)
     image = ToBinary().apply(image)
 
