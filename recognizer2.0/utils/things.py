b0bc56b8530c291bdff51355f9bee1a5e094f8ae
Laura Baakman <laurabaakman@gmail.com>
2016-06-23 16:58:23 +0200

    Defined a pixel class with neighbours.

--- recognizer2.0/utils/things.py
+++ recognizer2.0/utils/things.py
@@ -1,9 +1,43 @@
 from collections import namedtuple
+import math
 
 
 _RangeTuple = namedtuple('Range', ['min', 'max'], verbose=True)
 _SizeTuple = namedtuple('Size', ['width', 'height'], verbose=True)
+_Pixel = namedtuple('Pixel', ['row', 'column'])
 Point = namedtuple('Point', ['x', 'y'])
+
+
+class Pixel(_Pixel):
+
+    def manhattan_distance_to(self, other):
+        return abs((self.row - other.row)) + abs((self.column - other.column))
+
+    @property
+    def move_left(self):
+        return Pixel(row=self.row, column=self.column - 1)
+
+    @property
+    def move_right(self):
+        return Pixel(row=self.row, column=self.column + 1)
+
+    @property
+    def move_up(self):
+        return Pixel(row=self.row - 1, column=self.column)
+
+    @property
+    def move_down(self):
+        return Pixel(row=self.row + 1, column=self.column)
+
+    def neighbours_in(self, image):
+        if image.get_pixel(self.move_left) is not None:
+            yield self.move_left
+        if image.get_pixel(self.move_right) is not None:
+            yield self.move_right
+        if image.get_pixel(self.move_up) is not None:
+            yield self.move_up
+        if image.get_pixel(self.move_down) is not None:
+            yield self.move_down
 
 
 class Range(_RangeTuple):
