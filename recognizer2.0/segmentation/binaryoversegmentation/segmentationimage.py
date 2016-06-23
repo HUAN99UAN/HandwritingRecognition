ae1683cedb343520756adbc118f36c0718ac75c9
Laura Baakman <laurabaakman@gmail.com>
2016-06-23 14:54:25 +0200

    No need for copy, as the paint method copies the image.

--- recognizer2.0/segmentation/binaryoversegmentation/segmentationimage.py
+++ recognizer2.0/segmentation/binaryoversegmentation/segmentationimage.py
@@ -46,9 +46,11 @@ class SegmentationImage(Image):
     def show(self, wait_key=None, window_name=None, **kwargs):
         if not wait_key and not wait_key == 0:
             wait_key = super(SegmentationImage, self)._default_wait_key
-        image_with_ssp = copy.copy(self)
-        image_with_ssp = self._segmentation_lines.paint_on(image_with_ssp, **kwargs)
+        image_with_ssp = self._segmentation_lines.paint_on(self, **kwargs)
         image_with_ssp.show(wait_key=wait_key, window_name=window_name)
+
+    def dumps(self):
+        super(SegmentationImage, self).dumps()
 
     def __repr__(self):
         return "%s(%r)" % (self.__class__, self.__dict__)
