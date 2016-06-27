da94ecd5c6745b6fa51c96bd88a0a62ed33be4ea
Laura Baakman <laurabaakman@gmail.com>
2016-06-27 14:14:42 +0200

    Implementation of simple A-Star

--- recognizer2.0/segmentation/binaryoversegmentation/Astar.py
+++ recognizer2.0/segmentation/binaryoversegmentation/Astar.py
@@ -15,8 +15,10 @@ class AStar(object):
         self._start = start
         self._goal = goal
 
-        self._closed_set = set(start)
+        self._closed_set = list()
         self._open_set = set()
+        self._open_set.add(start)
+
         self._came_from = dict()
 
         self._heuristic = heuristic
@@ -32,49 +34,59 @@ class AStar(object):
         self._search()
 
     def _search(self):
-        while self._open_set:
+        while self._open_set and not self.has_path:
             current = self._get_current_node()
             self._review_node(current)
 
     def _get_current_node(self):
-        min_idx = np.argmin(
-            min(
-                self._closed_set,
-                key=lambda pixel : self._f_score.get(pixel, default=self._default_f_score)
-            )
-        )
-        current = self._closed_set.pop(min_idx)
-        self._open_set.add(current)
-        return current
+        current_pixel = min(self._open_set, key=lambda pixel: self._f_score.get(pixel, self._default_f_score))
+        self._open_set.remove(current_pixel)
+        self._closed_set.append(current_pixel)
+        return current_pixel
 
     def _review_node(self, node):
         for neighbour in node.neighbours_in(self._image):
+            if self.has_path:
+                break
             self._review_neighbour(node, neighbour)
 
     def _review_neighbour(self, node, neighbour):
-        if neighbour is self._goal:
-            self.retrace_path()
+        if neighbour == self._goal:
+            self.retrace_path(node)
+            return
 
-        tentative_score = self._g_score.get(node, self._default_g_score) + node.manhattan_distance_to(neighbour)
+        if neighbour in self._closed_set:
+            return
+
         self._open_set.add(neighbour)
-        if tentative_score < self._g_score.get(neighbour, self._default_g_score):
-            self._add_new_best_path(node, neighbour, score=tentative_score)
+        tentative_score = self._tentative_score(node=node, neighbour=neighbour)
+        if tentative_score >= self._g_score.get(neighbour, self._default_g_score):
+            return
+
+        self._add_new_best_path(node, neighbour, score=tentative_score)
 
     def _add_new_best_path(self, node, neighbour, score):
         self._came_from[neighbour] = node
         self._g_score[neighbour] = score
         self._f_score[neighbour] = self._g_score.get(neighbour) + self._heuristic(neighbour, self._goal)
 
+    def _tentative_score(self, node, neighbour):
+        return self._g_score.get(node) + node.manhattan_distance_to(neighbour)
+
     @property
     def path(self):
         return self._path
 
-    def retrace_path(self):
-        self._path.append(self._goal)
-        current = self._goal
+    @property
+    def has_path(self):
+        return bool(self._path)
+
+    def retrace_path(self, current):
+        path = [self._goal, current]
         while current in self._came_from.keys():
             current = self._came_from.get(current)
-            self._path.append(current)
+            path.append(current)
+        self._path = list(reversed(path))
 
     def __repr__(self):
         return "%s(%r)" % (self.__class__, self.__dict__)
@@ -98,4 +110,4 @@ if __name__ == '__main__':
     start = Pixel(row=3, column=2)
     end = Pixel(row=0, column=2)
 
-    AStar(image, start=start, goal=end, heuristic=heuristic)
+    print(AStar(image, start=start, goal=end, heuristic=heuristic).path)
