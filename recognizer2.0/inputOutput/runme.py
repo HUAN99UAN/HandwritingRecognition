85584360743cbcd14e6622287637ca1dc0c3b088
Angelo404 <e.karountzos@student.rug.nl>
2016-06-29 15:17:04 +0200

    Model kind of working now (we have the issue of the color mode still) + the pipe for the preprocessing is also done.

--- recognizer2.0/inputOutput/runme.py
+++ recognizer2.0/inputOutput/runme.py
@@ -1,9 +1,10 @@
 import wordio
 import argparse
 import actions
-import cv2
+from preprocessing import pipe
+from featureExtraction import crossings
+from os import path
 from utils import image
-from preprocessing import luminosity, morphological, otsu
 
 def parse_command_line_arguments():
     parser = argparse.ArgumentParser(description='Read the input for the classification of handwritten text.')
@@ -24,18 +25,17 @@ def parse_command_line_arguments():
 
 class _ModelBuilder(object):
 
-    def __init__(self, pre_processing_filter, feature_extractor, word_files, image_directory):
+    def __init__(self, feature_extractor, word_files, image_directory):
         """
 
-        :param pre_processing_filter:
         :param feature_extractor:
         :param word_files:
         :param image_directory:
         """
-        self._pre_processing_filter = pre_processing_filter
         self._feature_extractor = feature_extractor
         self._word_files = word_files
         self._image_directory = image_directory
+        self._model = {}
 
     def build(self, xmlfile):
         """
@@ -45,33 +45,51 @@ class _ModelBuilder(object):
         # TODO read words file, gives words, characters, and image_name
         words, characters, image_name = wordio.read(xmlfile)
 
-        # TODO create the path with the name of the file
-        image_path = self._image_directory+'/'+image_name
+        # TODO create the path with the name of the file PYTHON STUFF
+        image_path = path.join(self._image_directory, image_name + '.jpg')
 
         # TODO read image
-        img = cv2.imread(image_path)
+        img = image.Image.from_file(image_path)
 
         # TODO do pre-processing
-        img_after_luminosity = luminosity.HistogramsEqualization.apply(img)
-        img_after_binary = otsu.OtsuMethod.apply(img_after_luminosity)
-        img_after_morpho = morphological.Opening.apply(img_after_binary)
+        p = pipe.Pipe()
+        pre_processed_img = p.apply(img)
+
         for character in characters:
-            pass
+
             # TODO crop stuff
-            cropped_character = image.Image.sub_image(character)
+            cropped_character = pre_processed_img.sub_image(character)
             # TODO extract features
-            features = self._feature_extractor(cropped_character)
+            feature = self._feature_extractor.extract(cropped_character)
+            self.check_if_ch_exists(character.text)
         # TODO store in the model
 
 
 
+        # {'a': [feature_1, feature2]}
+
+    def check_if_ch_exists(self, ch, feature):
+        if ch in self._model:
+            self.update_ch(ch, feature)
+        else:
+            self.add_ch(ch, feature)
+
+    def add_ch(self, ch, feature):
+        self._model[ch] = [feature]
+
+    def update_ch(self, ch, feature):
+        self._model[ch] += feature
+
+
+
+
 if __name__ == '__main__':
-    arguments = parse_command_line_arguments()
+    #arguments = parse_command_line_arguments()
 
-    for xml in ['Stanford-test-40.words', 'Stanford-test-42.words',
-                'Stanford-test-44.words', 'Stanford-test-45.words',
-                'Stanford-test-46.words']:
-        tmp = wordio.read(xml)
-        break
+    m = _ModelBuilder(crossings.Crossings(), ['Stanford-CCCC_0092.words'], '/home/angelo/Documents/HandwritingRecognition/data/pages/Stanford')
+    m.build('Stanford-CCCC_0092.words')
+    # for xml in ['test.words']:
+    #     tmp = wordio.read(xml)
+    #     break
 
-    print tmp
+    # print tmp
