a53d8ce2f5dc8f8b8c3444fd0de0e214a372420e
Laura Baakman <laurabaakman@gmail.com>
2016-06-30 10:35:17 +0200

    Validation on character height, performance of the segmentation decreased.

--- recognizer2.0/segmentation/binaryoversegmentation/characterValidators.py
+++ recognizer2.0/segmentation/binaryoversegmentation/characterValidators.py
@@ -26,6 +26,16 @@ class ValidateOnWidth(_AbstractCharacterValidator):
         return (image.width >= self._minimum_character_width) and (image.width <= self._maximum_character_width)
 
 
+class ValidateOnHeight(_AbstractCharacterValidator):
+    def __init__(self, minimum_character_height, maximum_character_height):
+        super(ValidateOnHeight, self).__init__()
+        self._minimum_character_height = minimum_character_height
+        self._maximum_character_height = maximum_character_height
+
+    def is_valid(self, image):
+        return (image.height >= self._minimum_character_height) and (image.height <= self._maximum_character_height)
+
+
 class ValidateOnForegroundPixels(_AbstractCharacterValidator):
     def __init__(self, minimum_num_foreground_pixels):
         _AbstractCharacterValidator.__init__(self)
