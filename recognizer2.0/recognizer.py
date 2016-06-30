1277abbed70328a2940f15f50b888c67da7246c1
Laura Baakman <laurabaakman@gmail.com>
2016-06-30 20:49:16 +0200

    Changed the structure of the Recognizer class.

--- recognizer2.0/recognizer.py
+++ recognizer2.0/recognizer.py
@@ -12,41 +12,39 @@ from utils.image import Image
 
 class Recognizer(object):
 
-    def __init__(self, image, annotation,
+    def __init__(self,
                  preprocessor=preprocessing.Pipe(),
                  segmenter=segmentation.BinaryOverSegmentation(),
                  feature_extractor=featureExtraction.Crossings(),
                  classifier=classification.KNN(),
                  postprocessor=postprocessing.NearestLexiconEntry()):
-        self._image = preprocessor.apply(image)
-        self._annotation = annotation
+        self._pre_processor = preprocessor
         self._segmenter = segmenter
         self._feature_extractor = feature_extractor
         self._classifier = classifier
         self._post_processor = postprocessor
 
         self._output_lines = list()
-        self.recognize()
 
     @property
     def output_lines(self):
         return self._output_lines
 
-    def recognize(self):
-        for line in self._annotation:
-            output_line = self._recognize_line(line)
+    def recognize(self, image, annotation):
+        pre_processed_image = self._pre_processor.apply(image)
+        for line in annotation:
+            output_line = self._recognize_line(line, pre_processed_image)
             self._output_lines.append(output_line)
 
-    def _recognize_line(self, line):
+    def _recognize_line(self, line, image):
         output_line = list()
         for word in line:
-            word_image = self._image.sub_image(word)
+            word_image = image.sub_image(word)
             word_text = self._recognize_word(word_image)
             output_word = deepcopy(word)
             output_word.text = word_text
             output_line.append(output_word)
         return output_line
-
 
     def _recognize_word(self, word_image):
         character_images = self._segmenter.segment(word_image)
@@ -80,4 +78,7 @@ if __name__ == '__main__':
     annotation, _ = inputOutput.read(cli_arguments['words_file'])
     image = Image.from_file(cli_arguments['image'])
 
-    inputOutput.save(Recognizer(image=image, annotation=annotation).output_lines, cli_arguments['output_file'])
+    r = Recognizer(postprocessor=postprocessing.NearestLexiconEntryWithPrior())
+    r.recognize(image=image, annotation=annotation)
+
+    inputOutput.save(r.output_lines, cli_arguments['output_file'])
