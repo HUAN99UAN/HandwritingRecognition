961192f4af76f6e30ab765be58422ca6033c4072
Laura Baakman <laurabaakman@gmail.com>
2016-06-30 15:28:28 +0200

    New implemenattion of hamming distance.

---
+++ recognizer2.0/postprocessing/distancemeasures.py
@@ -0,0 +1,15 @@
+import scipy.spatial.distance as distances
+
+
+def hamming_distance(word_1, word_2):
+    length_difference = abs(len(word_1) - len(word_2))
+    differences = [
+        ord(character_1) != ord(character_2)
+        for (character_1, character_2)
+        in zip(word_1, word_2)
+    ]
+    return (sum(differences) + length_difference) / float(max(len(word_1), len(word_2)))
+
+
+def edit_distance(a, b):
+    raise NotImplementedError()
